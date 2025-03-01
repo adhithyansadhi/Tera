4f4158ae25882ea1f178a6c117ad6fbbc8c77765
adhithyansadhi <adhithyansadhi123@gmail.com>
2025-03-01 21:45:03 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Nav from "./components/Nanigation/Nav"; // Check spelling: "Nanigation" should be "Navigation"?
+import Nav from "./components/Navigation/Nav.jsx";
 import Contents from "./components/contents/Contents";
-import Contribute from "./components/contribute_tab/Contribute";
-import Getdata from "./components/Getdata/Getdata";
-import Adddata from "./components/AddData/Adddata";
-import Request from "./components/Request/Request";
+import Contribute from "./components/contribute_tab/Contribute.jsx";
+import Getdata from "./components/Getdata/Getdata.jsx";
+import Adddata from "./components/AddData/Adddata.jsx";
+import Request from "./components/request/Request.jsx";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 
@@ -12,17 +12,16 @@ function App() {
   return (
     <Router>
       {/* Navigation stays visible on all pages */}
-      <Nav />  
+      <Nav />
 
       <Routes>
         <Route path="/" element={<Contents />} />
         <Route path="/contribute" element={<Contribute />} />
         <Route path="/get" element={<Getdata />} />
-        <Route path="/Adddata" element={<Adddata />} />
+        <Route path="/adddata" element={<Adddata />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/request" element={<Request />} />
-        <Route path="/Home" element={<Home />} />
-        <Route path="/About" element={<About />} />
-
       </Routes>
     </Router>
   );
